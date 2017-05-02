13748180b858967d759374900522876bd71ef3d8
hud <29axe@users.noreply.github.com>
2017-05-02 14:21:20 +0200

    list of installed modules in code example

--- code/wsgi.py
+++ code/wsgi.py
@@ -2,13 +2,17 @@
 
 from flask import Flask
 import sys
+import pip
 
 
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def index():
-    return 'HotMaps-building_h-c' + ' running Python ' + sys.version + ', using virutalenv: ' + str(hasattr(sys, 'real_prefix'))
+    modules = 'List of installed modules:\n'
+    for i in pip.utils.get_installed_distributions(local_only=True):
+        modules = modules + '\n' + i.project_name + ' ' + i.version
+    return 'HotMaps-building_h-c' + ' running Python ' + sys.version + ', using virutalenv: ' + str(hasattr(sys, 'real_prefix')) + '\n<pre>' + modules + '</pre>'
 
 def test():
     application.run(debug=True)
