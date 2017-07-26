06a7388c4a83ed85b07bb759a3ec42eda0f057ea
fallahnejad <fallahnejad@eeg.tuwien.ac.at>
2017-07-26 17:49:07 +0200

    Update AD for F13, F63, F107 and HDMAP
    1. update test run outputs
    2. correction of relative path
    3. update of the name of output parameters by adding feature name prefix to the output name.

--- src/AD/F107/main.py
+++ src/AD/F107/main.py
@@ -1,31 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on July 11 2017
+Created on July 24 2017
 
 @author: fallahnejad@eeg.tuwien.ac.at
 """
 import os
 import sys
+from osgeo import gdal
 path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 if path not in sys.path:
     sys.path.append(path)
 from AD.heat_density_map.main import HDMAP
 
 
-def ad_f13():
+def ad_f107():
     # path to the AD
     path = os.path.dirname(os.path.dirname(__file__))
     data_warehouse = path + os.sep + 'data_warehouse'
-    heat_density_map = HDMAP(data_warehouse)
-    region = data_warehouse + os.sep + 'AT.shp'
-    # in GWh/km2
-    pix_threshold = 10
-    # in GWh/a
-    DH_threshold = 30
-    return (heat_density_map, region, pix_threshold, DH_threshold)
+    selected_area = data_warehouse + os.sep + 'AT312.shp'
+    hdm_path = HDMAP(data_warehouse)
+    hdm = gdal.Open(hdm_path)
+    return selected_area, hdm
 
 
 if __name__ == "__main__":
-    output = ad_f13()
-    for item in output:
-        print(item)
+    selected_area, hdm = ad_f107()
+    print(selected_area)
+    if isinstance(hdm, gdal.Dataset):
+        print('Heat density map loaded successfully!')
--- src/AD/F63_intern/main.py
+++ src/AD/F63_intern/main.py
@@ -10,28 +10,28 @@ def ad_f63(spec_demand_csv, building_strd_info_csv, inShapefile):
     [process0, process1, process2] = [True] * 3
     # path to the src
     path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    output_dir = path + os.sep + 'Outputs'
+    output_dir = os.getcwd() + os.sep + 'Outputs'
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
-    data_warehouse = path + os.sep + 'AD/data_warehouse'
+    data_warehouse = path + os.sep + 'AD/data_warehouse_intern'
     eu_shp = data_warehouse + os.sep + 'AT_NUTS3.shp'
     if spec_demand_csv == '':
         # average specific demand in EU countries in residential and service
         # sector
-        spec_demand_csv = data_warehouse + os.sep + 'useful_demand_AT.csv'
+        spec_demand_csv = data_warehouse + os.sep + 'useful_demand.csv'
         UsefulDemandRasterPath = data_warehouse
         UsefulDemandRaster = [UsefulDemandRasterPath +
-                              '/ResidentialUsefulDemand_AT.tif',
+                              '/ResidentialUsefulDemand.tif',
                               UsefulDemandRasterPath +
-                              '/ServiceUsefulDemand_AT.tif']
+                              '/ServiceUsefulDemand.tif']
         process0 = False
     else:
         UsefulDemandRasterPath = output_dir
-        UsefulDemandRaster = [output_dir + '/F63_ResidentialUsefulDemand_AT.tif',
-                              output_dir + '/F63_ServiceUsefulDemand_AT.tif']
-    outShapefile = output_dir + os.sep + 'F63_updated_building_footprint_AT.shp'
+        UsefulDemandRaster = [output_dir + '/ResidentialUsefulDemand.tif',
+                              output_dir + '/ServiceUsefulDemand.tif']
+    outShapefile = output_dir + os.sep + 'updated_building_footprint.shp'
     if building_strd_info_csv == '':
-        building_strd_info_csv = output_dir + os.sep + 'F63_building_strd_info.csv'
+        building_strd_info_csv = output_dir + os.sep + 'building_strd_info.csv'
     else:
         process1 = False
     if inShapefile == '':
@@ -43,7 +43,7 @@ def ad_f63(spec_demand_csv, building_strd_info_csv, inShapefile):
         process1 = False
     # to be added from data warehouse as soon as available
     population = 1000000
-    heatDensityRaster = output_dir + os.sep + 'F63_Heat_Density_Map.tif'
+    heatDensityRaster = output_dir + os.sep + 'Heat_Density_Map.tif'
     process_bool = (process0, process1, process2)
     inputValues = (eu_shp, spec_demand_csv, UsefulDemandRasterPath,
                    UsefulDemandRaster, inShapefile, building_strd_info_csv,
