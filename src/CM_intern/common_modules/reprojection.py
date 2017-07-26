7397dba05bd03f65c8fd87de48be0684aad4c2bd
Müller Andreas <andreasmueller284@gmail.com>
2017-07-26 17:06:51 +0200

    Remove of QGIS based reprojection
    Removed QGIS Version after profiling GDAL vs. QGIS

--- src/CM_intern/common_modules/reprojection.py
+++ src/CM_intern/common_modules/reprojection.py
@@ -4,52 +4,6 @@ from osgeo import ogr
 from osgeo import osr
 #import pandas as pd
 
-
-
-from qgis.core import *
-import sip
-import sys
-
-linux = "linux" in sys.platform
-if linux:
-    sys.path.append("/usr/share/qgis/python/plugins")
-
-API_NAMES = ["QDate", "QDateTime", "QString", "QTextStream", "QTime", "QUrl", "QVariant"]
-API_VERSION = 2
-for name in API_NAMES:
-    sip.setapi(name, API_VERSION)
-
-from PyQt4.QtGui import *
-#"""
-app = QgsApplication([], True)
-
-if linux:
-    app.setPrefixPath("/usr/share/qgis/")
-else:
-    app.setPrefixPath("C:/OSGeo4W64/apps/qgis", True)
-app.initQgis()
-
-
-import processing
-from processing.core.Processing import Processing
-Processing.initialize()
-Processing.updateAlgsList()
-#"""
-# ------------------------------------------------------------------------------------------------------------
-# --------------------------------------- Additional imports -------------------------------------------------
-
-
-def reprojection(inShapefile, outLayerPath, LayerName="DummyLayerName"):
-    suffiex = inShapefile[-4:].lower()
-    print("QGIS")
-    if suffiex == ".shp":
-        input_lyr = QgsVectorLayer(inShapefile, LayerName, "ogr")
-        print("Start reprojection Layer: %s -> %s" % (inShapefile, outLayerPath))
-        processing.runalg('qgis:reprojectlayer', input_lyr, "EPSG:3035", outLayerPath)
-        
-    else:
-        sys.exit("The input file is not valid!")
-
 def reprojectShp2Shp(inShapefile, outLayerPath
               , OutPutProjection=3035
               , LayerName="DummyLayerName"
@@ -57,7 +11,7 @@ def reprojectShp2Shp(inShapefile, outLayerPath
     
     
     # set geometry type before you run the program: wkbLineString, wkbPolygon, ...
-    print("GDAL")
+    # print("GDAL")
     driver = ogr.GetDriverByName('ESRI Shapefile')
 
     # get the input layer
