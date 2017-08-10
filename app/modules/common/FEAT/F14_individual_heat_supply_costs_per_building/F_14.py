ca6d7db000a0a5eb7f410ca65d220457835a06d8
Sara Fritz <fritz@eeg.tuwien.ac.at>
2017-08-10 15:46:46 +0200

    F14: Adaptation of structure
    -run_cm in corresponding calculation modules

--- app/modules/common/FEAT/F14_individual_heat_supply_costs_per_building/F_14.py
+++ app/modules/common/FEAT/F14_individual_heat_supply_costs_per_building/F_14.py
@@ -6,12 +6,17 @@ This script has been created in the context of the Hotmaps EU project.
 @Contact: fritz@eeg.tuwien.ac.at
 '''
 
+import os
 import sys
-sys.path.insert(0,'../..')
+path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.
+                                                       abspath(__file__))))
+if path not in sys.path:
+    sys.path.append(path)
 
 import AD.F14_input.main as data
-import CM.CM_TUW2.LCOH
-import CM.CM_TUW3.Co2_emissions
+
+import CM.CM_TUW2.run_cm as CM2
+import CM.CM_TUW3.run_cm as CM3
 
 import pandas
 import numpy
@@ -33,8 +38,8 @@ def execute():
     efficiency_heatingsystem=data.efficiency_heating_system['efficiency']
     spec_co2_emissions=pandas.DataFrame(numpy.transpose(data.specific_co2_emissions[data.specific_co2_emissions.columns[data.investment_costs_heating_system.idx_heatingsystem.values]].values)).T.squeeze()##T.squeeze generates an pandas series
                 
-    (var_costs, fix_costs,energy_costs, total_costs, share_of_taxes, co2_costs, lcoh, lcohcapita, fed) = CM.CM_TUW2.LCOH.levelized_costs_of_heat(energy_demand, heat_load,energy_price,co2_costs, taxes, investment_costs, operation_and_maintenance_costs, efficiency_heatingsystem,r, lt,population)
-    (specific_emissions, total_emissions) = CM.CM_TUW3.Co2_emissions.co2_emissions(energy_demand, efficiency_heatingsystem, spec_co2_emissions)
+    (var_costs, fix_costs,energy_costs, total_costs, share_of_taxes, co2_costs, lcoh, lcohcapita, fed) = CM2.main(energy_demand, heat_load,energy_price,co2_costs, taxes, investment_costs, operation_and_maintenance_costs, efficiency_heatingsystem,r, lt,population)
+    (specific_emissions, total_emissions) = CM3.main(energy_demand, efficiency_heatingsystem, spec_co2_emissions)
     
     name_heating_systems = (pandas.Series(data.investment_costs_heating_system['heating system'].values)).rename('Heating System')
     export_dataframe=pandas.concat([name_heating_systems,var_costs,fix_costs, energy_costs, total_costs, share_of_taxes, co2_costs, lcoh, lcohcapita, fed, specific_emissions, total_emissions],axis=1)
@@ -44,7 +49,15 @@ def execute():
 
 if __name__ == "__main__":
     print('calculation started')
+    output_dir = path + os.sep + 'Outputs'
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
     result = execute()
     print(result)
     print('calculation done')
+    
+    
+
+
+
     
