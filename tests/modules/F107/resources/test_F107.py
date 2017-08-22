fda0c76dab3f332bcc4f984809e0a8cc85a151ff
Sara Fritz <fritz@eeg.tuwien.ac.at>
2017-08-22 13:31:32 +0200

    generic tests for multiple payload<->result pairs

--- tests/modules/F107/resources/test_F107.py
+++ tests/modules/F107/resources/test_F107.py
@@ -1,37 +1,18 @@
-import json
-import requests
-import os
 import sys
+import os
 
 sys.path.append( os.path.join( os.path.dirname(os.path.abspath(__file__)), "..", "..", ".." ) )
 from test_settings import base_url
+from test_common import assert_equal_json, gen_tests
 
-def test_example_post():
-    """
-    This is an example method on how to do unit test on your API, you need to execute this using py.test in order to work
-    It will query the app and compare it with an already existing JSON file located in payload.txt
+url = base_url + "/buildinghc/f107"
 
-    $ pytest test_example.py
-    """
-    url = base_url + "/buildinghc/f107"
+path = os.path.dirname( os.path.abspath( __file__ ) )
+name = os.path.splitext( os.path.basename(__file__) )[0]
 
-    ######
-
-    path = os.path.dirname( os.path.abspath( __file__ ) )
-
-    # payload which is sent to server
-    payload = None
-    with open( os.path.join(path,"payload.json"), "r") as fd:
-        payload = json.load(fd)
-
-    # result which we expect
-    result = None
-    with open( os.path.join(path,"result.json"), "r") as fd:
-        result = json.load(fd)
-
-    # send payload to server
-    output = requests.post( url, json=payload)
-
-    # check if server answer matches out expectation
-    assert output.json() == result
-
+for test in gen_tests(\
+        name,\
+        path,\
+        url,\
+        'assert_equal_json' ):
+    exec( test )
--- tests/modules/example/resources/test_example.py
+++ tests/modules/example/resources/test_example.py
@@ -1,28 +1,18 @@
-import json
-import requests
+import sys
 import os
 
-def test_example_post():
-    """
-    This is an example method on how to do unit test on your API, you need to execute this using py.test in order to work
-    It will query the app and compare it with an already existing JSON file located in payload.txt
-    """
-    # TODO: set up the URL of your localhost (specially the port)
-    url = "http://localhost:9005/api/v1/example/hello/world"
+sys.path.append( os.path.join( os.path.dirname(os.path.abspath(__file__)), "..", "..", ".." ) )
+from test_settings import base_url
+from test_common import assert_equal_json, gen_tests
 
-    payload = "{\r\n  \"payload\": {\r\n    \"text\": \"This is a test\"\r\n  }\r\n}"
-    headers = {
-        'content-type': "application/json",
-        'cache-control': "no-cache",
-        'postman-token': "d188c7bd-5556-d8e0-33e2-f4c1d1abd817"
-    }
+url = base_url + "/buildinghc/f13"
 
-    output = requests.request("POST", url, data=payload, headers=headers)
+path = os.path.dirname( os.path.abspath( __file__ ) )
+name = os.path.splitext( os.path.basename(__file__) )[0]
 
-    json_file = os.path.join(os.path.dirname(__file__), "payload.txt")
-
-    json_str = open(json_file).read()
-
-    expected_output = json.loads(json_str)
-
-    assert output.json() == expected_output
+for test in gen_tests(\
+        name,\
+        path,\
+        url,\
+        'assert_equal_json' ):
+    exec( test )
